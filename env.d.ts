5b1530f69115c5a3b185a4fc92f93c978644e644
Denis Mainhardt <git@voqse.com>
2025-09-13 10:35:29 +0700

    Add Telegram SDK

--- env.d.ts
+++ env.d.ts
@@ -1,3 +1,11 @@
 /// <reference types="vite/client" />
 /// <reference types="vite-svg-loader" />
 /// <reference types="@intlify/unplugin-vue-i18n/messages" />
+
+import type { Telegram } from '@twa-dev/types'
+
+declare global {
+  interface Window {
+    Telegram: Telegram
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
+import WebApp from '@twa-dev/sdk'
 import { createPinia } from 'pinia'
 import { createApp } from 'vue'
-
 import i18n, { setLocale } from '@/i18n'
 import App from './App.vue'
 import router from './router'
+
+WebApp.ready()
 
 const app = createApp(App)
 
