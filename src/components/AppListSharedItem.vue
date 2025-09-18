a30d81e0583ff3579021374043ef2ae8d0838aab
Denis Mainhardt <git@voqse.com>
2025-09-18 09:40:18 +0700

    Tune styles

--- src/components/AppListSharedItem.vue
+++ src/components/AppListSharedItem.vue
@@ -1,36 +1,40 @@
 <script setup lang="ts">
 import type { Item } from '@/api/types'
+import { computed } from 'vue'
 import { itemReserve, itemReserveCancel } from '@/api'
 import style from '@/assets/scss/base.module.scss'
 import AppCheckbox from '@/components/AppCheckbox.vue'
 import AppUserPhoto from '@/components/AppUserPhoto.vue'
 import { formatUsername, linkify } from '@/utils'
 
-defineProps<{
+const props = defineProps<{
   item: Item
   disabled?: boolean
 }>()
+
+const isReserved = computed(() => Boolean(props.item.reservedBy))
 </script>
 
 <template>
   <li :class="style.appListItem">
     <AppCheckbox
       :class="style.appListItemCheckboxFullWidth"
-      :model-value="Boolean(item.reservedBy)"
+      :model-value="isReserved"
       :disabled
       @update:model-value="$event ? itemReserve(item.id) : itemReserveCancel(item.id)"
     >
       <template #photo>
         <AppUserPhoto
-          v-if="Boolean(item.reservedBy) && item.reservedBy?.photoUrl"
+          v-if="isReserved && item.reservedBy?.photoUrl"
           :src="item.reservedBy.photoUrl"
           :alt="`${item.reservedBy.username}'s photo`"
           :class="style.appCheckBoxPhoto"
         />
       </template>
+
       <template #default>
         <div :class="style.appListItemText" v-html="linkify(item.text)" />
-        <div v-if="Boolean(item.reservedBy)" :class="style.appListItemReserverUsername">
+        <div v-if="isReserved" :class="style.appListItemReserverUsername">
           {{ formatUsername(item.reservedBy!) }}
         </div>
       </template>
