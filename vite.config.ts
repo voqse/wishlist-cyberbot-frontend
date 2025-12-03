398563c83c5dcc7161147082ec7292abe72de0e8
Denis Mainhardt <git@voqse.com>
2025-12-03 12:59:13 +0700

    Fix matt glass

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import VueI18nPlugin from '@intlify/unplugin-vue-i18n/vite'
 import basicSsl from '@vitejs/plugin-basic-ssl'
 import vue from '@vitejs/plugin-vue'
 import { defineConfig, loadEnv } from 'vite'
-import vueDevTools from 'vite-plugin-vue-devtools'
 import svgLoader from 'vite-svg-loader'
 
 const resolve = (path: string) => fileURLToPath(new URL(path, import.meta.url))
@@ -27,7 +26,7 @@ export default defineConfig(({ mode }) => {
     },
     plugins: [
       vue(),
-      vueDevTools(),
+      // vueDevTools(),
       basicSsl(),
       svgLoader(),
       VueI18nPlugin({
